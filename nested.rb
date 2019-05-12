ddd8598509f1c52ab8933389221c3c1816e0ea40
Jasmine Chen <jasmine1226@gmail.com>
2019-05-12 22:17:16 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -55,8 +55,8 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-		dennis_ritchies_language = ""
-	return dennis_ritchies_language << programmer_hash[:dennis_ritchie][:languages]
+
+	return programmer_hash[:dennis_ritchie][:languages].to_s
 end
 
 def adding_matz
